94c703664e0888646cfb71bcf1789c6bcb30d501
JahirBautistaQ <02210132008@mail.udes.edu.co>
2025-04-08 01:19:32 -0500

    Modulo 1: Se añadio la clase Asistencia la cual es la ultima identificada del modulo 1. Tambien se le añadio un enum a dicha clase para tener estados como PRESENTE, AUSENTE, JUSTIFICADO.

---
+++ src/main/java/co/edu/udes/backend/models/Asistencia.java
@@ -0,0 +1,81 @@
+package co.edu.udes.backend.models;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity(name = "asistencias")
+public class Asistencia {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "estudiante_id")
+    @JsonBackReference
+    private Estudiante estudiante;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "curso_id")
+    @JsonBackReference
+    private Curso curso;
+
+    @Column(name = "fecha")
+    private LocalDate fecha;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "estado")
+    private EstadoAsistencia estado;
+
+    public Asistencia() {
+    }
+
+    public Asistencia(long id, Estudiante estudiante, Curso curso, LocalDate fecha, EstadoAsistencia estado) {
+        this.id = id;
+        this.estudiante = estudiante;
+        this.curso = curso;
+        this.fecha = fecha;
+        this.estado = estado;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Estudiante getEstudiante() {
+        return estudiante;
+    }
+
+    public void setEstudiante(Estudiante estudiante) {
+        this.estudiante = estudiante;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public EstadoAsistencia getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoAsistencia estado) {
+        this.estado = estado;
+    }
+}
---
+++ src/main/java/co/edu/udes/backend/models/EstadoAsistencia.java
@@ -0,0 +1,7 @@
+package co.edu.udes.backend.models;
+
+public enum EstadoAsistencia {
+    PRESENTE,
+    AUSENTE,
+    JUSTIFICADO
+}
